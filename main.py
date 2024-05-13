1bb684cf58cea63b20e3263d2baa18625bcc13a3
M.Rizqullah <mm.rizqullah@gmail.com>
2024-05-13 20:02:24 +0800

    add project

---
+++ main.py
@@ -0,0 +1,86 @@
+import os
+import pandas as pd
+
+pertanyan = input('''
+Pilih pertanyaan mu
+1.Berapa Banyak Transaksi yang terjadi di bulan Mei?
+
+2.Berapa Banyak Transaksi yang terjadi di tanggal 29 Mei?
+
+3. 3 pelanggan yang paling banyak jumlah transaksinya?
+
+4.(Sesuai no 3) Berapa banyak transaksi 3 pelanggan tersebut, masing-masing?
+
+5. 3 Produk yang paling banyak dibeli?
+
+6. (Sesuai no 5) Berapa banyak penjualan 3 produk tersebut, masing-masing?
+
+7. 3 Produk yang paling sedikit dibeli?
+
+8. (Sesuai no 7) Berapa banyak penjualan 3 produk tersebut, masing-masing?
+
+9. 3 pelanggan yang paling banyak nominal pembeliannya? (paling banyak Rp)
+
+10. (Sesuai no 9) Berapa rupiah pembelian masing2 pelanggan tersebut?
+
+11. Tanggal Berapa Transaksi Terbanyak?
+
+12. Tanggal Berapa Pembelian Produk Terbanyak?
+
+13. Jika Pelanggan Prioritas adalah Pelanggan yang berbelanja setidaknya 8 kali dalam sebulan, sebutkan ada berapa banyak pelanggan yang akan diprioritaskan?
+
+14. Dari Daerah Mana saja Pelanggan Prioritas tersebut?
+
+15. Rata-rata, hari apa Jumlah Transaksi Terbanyak? (3 hari)
+
+16. Rata-rata, hari apa Pelanggan Datang Paling Banyak?  (3 hari)
+
+17. Rata-rata, hari apa Jumlah Nominal Pembelian paling tinggi?  (3 hari)
+
+18. Pelanggan dengan nama Putri suka berbelanja di hari apa?
+
+19. 3 Daerah Berpotensi untuk membuka cabang?
+
+20. Jelaskan alasan kalian memilih daerah tersebut!
+
+21. Adakah 2 barang yang bisa dijual bersamaan?
+
+22. Alasan anda memilih dua barang tersebut dijual bersamaan?
+
+23. Adakah 3 barang yang bisa dijual bersamaan?
+
+24. Alasan anda memilih dua barang tersebutdijual bersamaan?
+
+25. Jika ada teknik promosi yang baik untuk meningkatkan penjualan, berikan 3 contoh teknik promosi yang menurut anda cocok!
+
+Berikan no berapa yang ingin di tampilkan :
+''')
+
+if pertanyan == '1':
+elif pertanyan == '2':
+elif pertanyan =='3':
+elif pertanyan == "4":
+elif pertanyan == "5":
+elif pertanyan == '6':
+elif pertanyan == "7":
+elif pertanyan == "8":
+elif pertanyan == "9":
+elif pertanyan == "10":
+elif pertanyan == "11":
+elif pertanyan == "11":
+    elif pertanyan == "11":
+        elif pertanyan == "11":
+            elif pertanyan == "11":
+                elif pertanyan == "11":
+                    elif pertanyan == "11":
+                        elif pertanyan == "11":
+                            elif pertanyan == "11":
+                                elif pertanyan == "11":
+                                    elif pertanyan == "11":
+                                        elif pertanyan == "11":
+                                            elif pertanyan == "11":
+                                                elif pertanyan == "11":
+                                                    elif pertanyan == "11":
+                                                        
+else:
+    print("Nomor tidak falid")
